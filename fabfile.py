3640321b0c027d9dcc7b18b063e7952fdd51bd03
kencochrane <kencochrane@localhost>
2011-06-26 21:32:26 -0400

    added fabric file

---
+++ fabfile.py
@@ -0,0 +1,29 @@
+from __future__ import with_statement
+from fabric.api import *
+from fabric.contrib.console import confirm
+from fabric.contrib.files import exists
+
+def update_server():
+    code_dir = '/opt/django/apps/my_app/current/my_app/'
+    with settings(user='django'):
+        with cd(code_dir):
+            # pull down and update, could be replaced with hg fetch if turned on.
+            run("hg pull && hg up")
+            
+            # change ownership to django just to be safe.
+            run("chown -R django:django *")
+            
+            # if you have a database setup
+            #run("/opt/django/apps/my_app/current/bin/python2.6 manage.py syncdb")
+            
+            # if you have south setup to manage your migrations
+            #run("/opt/django/apps/my_app/current/bin/python2.6 manage.py migrate")
+            
+            # remove all pyc files
+            run('find -name "*.pyc" -delete')
+            
+            # restart the app
+            run("supervisorctl restart myapp")
+            
+            # print status to make sure it restarted correctly
+            run('supervisorctl status')
